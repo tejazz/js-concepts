c08400fa176f70d88fd73eb8e421111e907de78e
Tarique <tariqueejaz.93102@gmail.com>
2021-12-02 00:58:44 +0100

    feat: call, apply, bind

--- javascript-foundations-ii/this.js
+++ javascript-foundations-ii/this.js
@@ -36,3 +36,49 @@ const obj2 = {
 importantPerson();          // Sunny
 obj1.importantPerson();     // Cassy
 obj2.importantPerson();     // Jacob
+
+// Lexical Scope is not the same when it comes to usage of 'this'
+
+const obj = {
+    name: 'Billy',
+    sing: function() {
+        console.log('a', this);     // refers to object 'obj'
+        function anotherFunc() {
+            console.log('b', this);
+        }
+
+        anotherFunc();              // refers to the global object since it is called without specific context
+    }
+}
+
+// arrows functions are lexically binding
+const obj = {
+    name: 'Billy',
+    sing: function() {
+        console.log('a', this);     // refers to object 'obj'
+        var anotherFunc = () => {
+            console.log('b', this);
+        }
+
+        anotherFunc();              // refers to object 'obj'
+    }           
+}
+
+// Call, Apply, Bind
+
+const wizard = {
+    name: 'Merlin',
+    health: 100,
+    heal() {
+        return this.health = 100
+    }
+}
+
+const archer = {
+    name: 'Robin Hood',
+    health: 30,
+}
+
+console.log('1', archer);   // health: 30
+wizard.heal.call(archer);   // calls the heal() of wizard with archer object
+console.log('2', archer);   // health: 100
