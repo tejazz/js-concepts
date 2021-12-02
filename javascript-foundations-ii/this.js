8861125b1ecdb418a27b42abb13f948ea6643f40
Tarique <tariqueejaz.93102@gmail.com>
2021-12-02 01:13:22 +0100

    feat: this keyword

--- javascript-foundations-ii/this.js
+++ javascript-foundations-ii/this.js
@@ -2,10 +2,10 @@
 
 const obj = {
     name: 'Billy',
-    sing: function() {
+    sing: function () {
         return 'lalala' + this.name     // this refers to the object 'obj' sing() is a property of
     },
-    singAgain: function() {
+    singAgain: function () {
         return this.sing() + '!'
     }
 }
@@ -41,7 +41,7 @@ obj2.importantPerson();     // Jacob
 
 const obj = {
     name: 'Billy',
-    sing: function() {
+    sing: function () {
         console.log('a', this);     // refers to object 'obj'
         function anotherFunc() {
             console.log('b', this);
@@ -54,14 +54,14 @@ const obj = {
 // arrows functions are lexically binding
 const obj = {
     name: 'Billy',
-    sing: function() {
+    sing: function () {
         console.log('a', this);     // refers to object 'obj'
         var anotherFunc = () => {
             console.log('b', this);
         }
 
         anotherFunc();              // refers to object 'obj'
-    }           
+    }
 }
 
 // Call, Apply, Bind
@@ -82,3 +82,35 @@ const archer = {
 console.log('1', archer);   // health: 30
 wizard.heal.call(archer);   // calls the heal() of wizard with archer object
 console.log('2', archer);   // health: 100
+
+// Exercise
+
+const array = [1, 2, 3];
+
+function getMaxNumber(arr) {
+    return Math.max.apply(null, arr);   // 'this' does not matter here
+}
+
+getMaxNumber(array) // should return 3
+
+// Function Currying
+function multiply(a, b) {
+    return a * b;
+}
+
+let multiplyByTwo = multiply.bind(this, 2);
+console.log(multiplyByTwo(4));      // 8
+
+// Exercise
+
+const character = {
+    name: 'Simon',
+    getCharacter() {
+        return this.name;
+    }
+};
+
+// bind to the object 'character'
+const giveMeTheCharacterNOW = character.getCharacter.bind(character);
+
+console.log('?', giveMeTheCharacterNOW());
